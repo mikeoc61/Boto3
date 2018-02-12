ecdc4ecbe07aae2254d2acefd238d0f3ce8a4d24
Michael OConnor <gmikeoc@gmail.com>
2018-02-12 17:20:58 -0600

    significant rewrite

--- s3ls.py
+++ s3ls.py
@@ -4,61 +4,74 @@
 # passed a command line argument, list the contents of that specific bucket
 
 __author__      = "Michael E. O'Connor"
-__copyright__   = "Copyright 2017"
+__copyright__   = "Copyright 2018"
 
 import sys
 import boto3
 import botocore
 
-def s3ls (target):
 
-    if target == "ALL":
-        s3 = boto3.client('s3')
-        # Call S3 to list current buckets
-        response = s3.list_buckets()
-        buckets = [bucket['Name'] for bucket in response['Buckets']]
-#        print("Bucket List: %s" % buckets)
-        for bucket in response['Buckets']:
-            print bucket['Name']
-    else:
-        s3 = boto3.resource('s3')
-        bucket = s3.Bucket(target)
-        for obj in bucket.objects.all():
-            print obj.key
+# Display list of file all S3 buckets associated with user credentials
 
-    return 1
+def s3ls_all():
+
+   s3 = boto3.client('s3')
+   response = s3.list_buckets()
+#  buckets = [bucket['Name'] for bucket in response['Buckets']]
+   for bucket in response['Buckets']:
+       print bucket['Name']
+
+   return 1
+
+# Display list of file objects associated with provided S3 bucket name
+
+def s3ls_specific (target):
+
+   s3 = boto3.resource('s3')
+   bucket = s3.Bucket(target)
+   for obj in bucket.objects.all():
+       print obj.key
+
+   return 1
 
 
 def main():
 
     my_session = boto3.session.Session()
+#    print ("Current Default Region: [%s]" % my_session.region_name)
 
-    if len(sys.argv) >= 2:          # User provided an S3 bucket name
-        arg = sys.argv[1]
-    else:                           # Provide listing of all available buckets
-        arg = "ALL"
-        print ("Listing S3 Buckets in Region: [%s]" % my_session.region_name)
-        return (s3ls(arg))
+    # Check to see if bucket name was provided on command line
+    # If not then display a listing of all buckets owned by user
+
+    if len(sys.argv) == 1:
+        print ("Listing all S3 Buckets owned by user associated with access keys")
+        return (s3ls_all())
 
     # Since we are looking to list the contents of a specific S3 bucket
     # we need to check to see if the bucket name is valid and readable
     # before proceeding
 
+    if len(sys.argv) == 2: 
+        arg = sys.argv[1]
+    else:
+        print ("Sorry, this sad little program only accepts a single command line argument")
+        return 0
+
     s3 = boto3.resource('s3')
-    success = True
 
     try:
         s3.meta.client.head_bucket(Bucket=arg)
+
     except botocore.exceptions.ClientError as e:
         error_code = int(e.response['Error']['Code'])
-        success = False
-
-    if success:
-        return (s3ls(arg))            # All clear so get list of bucket contents
-    else:
-        print ("Error Code [%s]: Bucket name [%s] is not accessible" % (error_code, arg))
+        print ("HTTP Error Code [%s]: Bucket name [%s] is not accessible" % (error_code, arg))
         print ("See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes")
         return 0
+
+    # Made it this far so let's print out a list of file objects associated with this bucked name
+
+    print ("Searching for readable s3 buckets matching name: [%s]" % arg)
+    return (s3ls_specific(arg))            # All clear so get list of bucket contents
 
 if __name__ == '__main__':
   main()
